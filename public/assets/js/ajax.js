34dd18cc5fd963a00c6a51823c5929d6132b2ce0
Soaryn <murilo.conde@fatec.sp.gov.br>
2019-05-15 12:32:53 -0300

    updated funcionality and error treatment

--- public/assets/js/ajax.js
+++ public/assets/js/ajax.js
@@ -14,16 +14,44 @@ function loadUserLocation() {
     xhttploc.send();
 }
 
+function emptyResponse() {
+    document.getElementById("cidade").innerHTML = '-';
+    document.getElementById("clima").innerHTML = '-';
+    document.getElementById("temp").innerHTML = '-';
+    document.getElementById("pokemon").innerHTML = '-';
+    document.getElementById("imgtipo").src = "/assets/img/types/normal.png";
+    document.getElementById("pokeImg").src = "";
+    document.getElementById("hp").innerHTML = '-';
+    document.getElementById("attack").innerHTML = '-';
+    document.getElementById("defense").innerHTML = '-';
+    document.getElementById("sattack").innerHTML = '-';
+    document.getElementById("sdefense").innerHTML = '-';
+    document.getElementById("speed").innerHTML = '-';
+    document.getElementById("altura").innerHTML = '-';
+    document.getElementById("peso").innerHTML = '-';
+    document.getElementById("modalPokemon").innerHTML = '-';
+    document.getElementById("erroModal").style.display = 'flex';
+}
+
 function loadInfo(lon, lat) {
 
-    document.getElementById('overlay').style.display = 'flex'
+    document.body.style.overflow = 'hidden';
+    document.getElementById('overlay').style.display = 'flex';
+    timeout = setTimeout(() => {
+        document.getElementById('overlay').style.display = 'none';
+        emptyResponse();
+        xhttp.abort();
+    }, 10000);;
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
 
         if (this.readyState == 4 && this.status == 200) {
 
+            clearTimeout(timeout);
+
             document.getElementById("imgstipo").innerHTML = "<p>Tipos</p>";
+            document.getElementById("moves").innerHTML = "";
 
             res = JSON.parse(this.responseText);
 
@@ -32,11 +60,7 @@ function loadInfo(lon, lat) {
             pokemon = JSON.parse(res.resPokemon.text);
 
             if (res.resWeather.status != 200) {
-                document.getElementById("cidade").innerHTML = '-';
-                document.getElementById("clima").innerHTML = '-';
-                document.getElementById("temp").innerHTML = '-';
-                document.getElementById("pokemon").innerHTML = '-';
-                alert('local não encontrado')
+                emptyResponse();
             } else {
                 document.getElementById("cidade").innerHTML = weather.name ? weather.name : 'Localizacao sem nome';
                 document.getElementById("clima").innerHTML = weather.weather[0].description.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
@@ -51,28 +75,29 @@ function loadInfo(lon, lat) {
                 document.getElementById("sattack").innerHTML = pokemon.stats[2].base_stat;
                 document.getElementById("sdefense").innerHTML = pokemon.stats[1].base_stat;
                 document.getElementById("speed").innerHTML = pokemon.stats[0].base_stat;
-                document.getElementById("altura").innerHTML = (pokemon.height/10.0) + "m";
-                document.getElementById("peso").innerHTML = (pokemon.weight/10.0) + "kg";
+                document.getElementById("altura").innerHTML = (pokemon.height / 10.0) + "m";
+                document.getElementById("peso").innerHTML = (pokemon.weight / 10.0) + "kg";
 
                 for (var i = 0; i < 4; i++) {
-                  var move = document.createElement("DIV");
-                  move.innerHTML= pokemon.moves[i].move.name;
-                  move.style.padding = '.5em';
-                  move.style.boxSizing = 'border-box';
-                  move.style.textTransform = 'uppercase';
-                  move.style.width = '50%';
 
-                  document.getElementById('moves').appendChild(move);
+                    var num = Math.floor(Math.random() * pokemon.moves.length);
+
+                    var move = document.createElement("DIV");
+                    move.innerHTML = pokemon.moves[num].move.name;
+                    move.classList.add('move');
+
+                    document.getElementById('moves').appendChild(move);
                 }
 
                 pokemon.types.forEach(function (element) {
-                  var img = document.createElement("IMG");
-                  img.src = "/assets/img/types/" + element.type.name + ".png";
-                  document.getElementById("imgstipo").appendChild(img);
+                    var img = document.createElement("IMG");
+                    img.src = "/assets/img/types/" + element.type.name + ".png";
+                    document.getElementById("imgstipo").appendChild(img);
                 })
             }
 
-            document.getElementById('overlay').style.display = 'none'
+            document.body.style.overflow = 'auto';
+            document.getElementById('overlay').style.display = 'none';
 
             document.getElementById('info').scrollIntoView({
                 behavior: 'smooth'
