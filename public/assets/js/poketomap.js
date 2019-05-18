058e0368f2b902cbca3b6ae4625e8ad13c42a271
Soaryn <murilo.conde@fatec.sp.gov.br>
2019-05-18 12:49:29 -0300

    bugfix

--- public/assets/js/poketomap.js
+++ public/assets/js/poketomap.js
@@ -5,30 +5,45 @@ function addPokeToMap(pokemon, lon, lat) {
 
   map.loadImage(pokemon.sprites.front_default, function(error, image) {
     if (error) {
-      throw error;
-    }
-    if (!map.hasImage(pokemon.name)) {
-      map.addImage(pokemon.name, image);
-    }
-
-    pokemons.push({
-      "type": "Feature",
-      "properties": {
-        "description": "<strong style='text-transform: uppercase'>" + pokemon.name + "</strong>",
-        "icon": pokemon.name,
-        "pokeid": pokeId++
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [lon, lat]
+      pokemons.push({
+        "type": "Feature",
+        "properties": {
+          "description": "<strong style='text-transform: uppercase'>" + pokemon.name + "</strong>",
+          "pokeid": pokeId++
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [lon, lat]
+        }
+      });
+  
+      map.getSource("pokemons").setData({
+        "type": "FeatureCollection",
+        "features": pokemons
+      });
+    } else {
+      if (!map.hasImage(pokemon.name)) {
+        map.addImage(pokemon.name, image);
       }
-    });
-
-    map.getSource("pokemons").setData({
-      "type": "FeatureCollection",
-      "features": pokemons
-    });
-
+  
+      pokemons.push({
+        "type": "Feature",
+        "properties": {
+          "description": "<strong style='text-transform: uppercase; font-size: 18px'>" + pokemon.name + "</strong>",
+          "icon": pokemon.name,
+          "pokeid": pokeId++
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [lon, lat]
+        }
+      });
+  
+      map.getSource("pokemons").setData({
+        "type": "FeatureCollection",
+        "features": pokemons
+      });
+    }
 
   })
 
