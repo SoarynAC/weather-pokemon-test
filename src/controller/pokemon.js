39fe326391ab932deb8e986ab95ad9ba8e58e1fb
Soaryn <murilo.conde@fatec.sp.gov.br>
2019-05-20 23:51:10 -0300

    filtered out new generation pokemons

--- src/controller/pokemon.js
+++ src/controller/pokemon.js
@@ -2,19 +2,22 @@ var request = require('superagent')
 
 module.exports = async function (list) {
 
-    num = Math.floor(Math.random() * list.body.pokemon.length)
+    do {
 
-    while (list.body.pokemon[num].pokemon.name.indexOf("-") !== -1){
-        num = Math.floor(Math.random() * list.body.pokemon.length)
-    }    
+        do {
+            num = Math.floor(Math.random() * list.body.pokemon.length)
+        } while (list.body.pokemon[num].pokemon.name.indexOf("-") !== -1)
 
-    pokemon = await request.get(list.body.pokemon[num].pokemon.url)
-        .then(function (res) {
-            return res
-        })
-        .catch(function (err) {
-            console.log(err)
-            return err.response
-        })
+        pokemon = await request.get(list.body.pokemon[num].pokemon.url)
+            .then(function (res) {
+                return res
+            })
+            .catch(function (err) {
+                console.log(err)
+                return err.response
+            })
+
+    } while (pokemon.body.id > 721)
+
     return pokemon
 }
