5ba2ab7200d8c7cef7f49886e7ad4c6e0056c3fc
Soaryn <murilo.conde@fatec.sp.gov.br>
2019-05-25 19:34:18 -0300

    bugfix

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'poke-cache-v1'
+const CACHE_NAME = 'poke-cache-v2'
 var urlsToCache = [
-    '/',
+    '/favicon.ico',
     '/assets/css/style.css',
     '/assets/js/ajax.js',
     '/assets/js/map.js',
@@ -10,15 +10,32 @@ var urlsToCache = [
     '/assets/js/poketomap.js',
     '/assets/js/tz.js',
     '/assets/font/pokemon_pixel_font.ttf',
-    '/assets/img/',
-    '/assets/img/types/'
+    '/assets/img/bg.png',
+    '/assets/img/logo.png',
+    '/assets/img/types/bug.png',
+    '/assets/img/types/dark.png',
+    '/assets/img/types/dragon.png',
+    '/assets/img/types/electric.png',
+    '/assets/img/types/fairy.png',
+    '/assets/img/types/fighting.png',
+    '/assets/img/types/ghost.png',
+    '/assets/img/types/fire.png',
+    '/assets/img/types/flying.png',
+    '/assets/img/types/grass.png',
+    '/assets/img/types/ground.png',
+    '/assets/img/types/ice.png',
+    '/assets/img/types/normal.png',
+    '/assets/img/types/poison.png',
+    '/assets/img/types/psychic.png',
+    '/assets/img/types/rock.png',
+    '/assets/img/types/steel.png',
+    '/assets/img/types/water.png'
 ]
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
         .then(function (cache) {
-            console.log('Opened cache')
             return cache.addAll(urlsToCache)
         })
     )
